e972fa74161a55ea2a020d3d8eda436e4bb6eeb9
Akin Aguda <lordtitan7@gmail.com>
2019-06-05 04:36:06 +0100

    adjusted console log

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const path = require('path');
 const fs = require('fs');
 fs.readdir(__dirname, (err, files) => {
     files.forEach(file => {
-        console.log('SORTING FILES');
+        console.log('SORTING FILE '+file);
         const arr = file.match(/[a-zA-z][\d]-[\d][\d]/g);
         if (arr !== null) {
             fs.rename(__dirname+'/'+file, __dirname+'/'+arr[0].split('-')[1]+'-'+file, error => {
                 if(error) throw err;
-                console.log('FILES SORTED');
             })
         }
+        console.log('FILES SORTED');
     });
 })
