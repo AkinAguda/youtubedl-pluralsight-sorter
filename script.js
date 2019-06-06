6bc5acfaa742ee5168ba6209b66ff0ef4749d2d6
Akin Aguda <lordtitan7@gmail.com>
2019-06-06 23:18:04 +0100

    removed redundant code

--- script.js
+++ script.js
@@ -27,19 +27,3 @@ fs.readdir(__dirname, (err, files) => {
         }
     }
 })
-const sortFiles = () => {
-    fs.readdir(__dirname, (err, files) => {
-        files.forEach(file => {
-            console.log('SORTING FILE...\n '+file);
-            const arr = file.match(/[a-zA-z][\d]-[\d][\d]/g);
-            if (arr !== null) {
-                fs.rename(__dirname+'/'+file, __dirname+'/'+arr[0].split('-')[1]+'-'+file, error => {
-                    if(error) throw err;
-                })
-            }
-            console.log('FILE SORTED');
-        });
-    })
-}
-
-
