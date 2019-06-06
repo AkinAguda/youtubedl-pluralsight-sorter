6c4bfab44d4bd1c9f2d561ce1918cdafb6cf85fe
Akin Aguda <lordtitan7@gmail.com>
2019-06-06 23:17:12 +0100

    now sorts into folders

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const path = require('path');
 const fs = require('fs');
+
 fs.readdir(__dirname, (err, files) => {
+    const chaps = [];
     files.forEach(file => {
-        console.log('SORTING FILE...\n '+file);
         const arr = file.match(/[a-zA-z][\d]-[\d][\d]/g);
         if (arr !== null) {
-            fs.rename(__dirname+'/'+file, __dirname+'/'+arr[0].split('-')[1]+'-'+file, error => {
-                if(error) throw err;
+            chaps.push(arr[0].split('-')[0].split('')[1])
+        }
+    })
+    chaps.sort();
+    for (i = 1; i <= chaps[chaps.length - 1]; i++) {
+        if (fs.existsSync(__dirname+'/m'+i)) {
+            continue;
+        }else {
+            fs.mkdirSync(__dirname+'/m'+i)
+            const regex = new RegExp('m'+(i)+'-[0-9][0-9]');
+            files.forEach(file => {
+                const arr = file.match(regex);
+                if (arr) {
+                    fs.rename(__dirname+'/'+arr.input, __dirname+'/m'+(i)+'/'+arr[0].split('-')[1]+'-'+file, error => {
+                        if(error) throw err;
+                    })
+                }
             })
         }
-        console.log('FILE SORTED');
-    });
+    }
 })
+const sortFiles = () => {
+    fs.readdir(__dirname, (err, files) => {
+        files.forEach(file => {
+            console.log('SORTING FILE...\n '+file);
+            const arr = file.match(/[a-zA-z][\d]-[\d][\d]/g);
+            if (arr !== null) {
+                fs.rename(__dirname+'/'+file, __dirname+'/'+arr[0].split('-')[1]+'-'+file, error => {
+                    if(error) throw err;
+                })
+            }
+            console.log('FILE SORTED');
+        });
+    })
+}
+
+
