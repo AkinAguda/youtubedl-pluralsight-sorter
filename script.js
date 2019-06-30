f84b9778c812a7b224eded7cfca49695c1a2a505
Akin Aguda <lordtitan7@gmail.com>
2019-06-30 02:16:30 +0100

    added case for new pluralsight video format

--- script.js
+++ script.js
@@ -4,38 +4,66 @@ const fs = require("fs");
 fs.readdir(__dirname, (err, files) => {
   console.log("Sorting Files into folders...");
   const chaps = [];
+  let nullCount = 0;
   files.forEach(file => {
     const arr = file.match(/m[\d]*-[\d]*/g);
     if (arr !== null) {
       chaps.push(arr[0].split("-")[0].slice(1));
+    } else {
+      nullCount++;
     }
   });
-  chaps.sort();
-  for (i = chaps[0]; i <= chaps[chaps.length - 1]; i++) {
-    if (fs.existsSync(path.join(__dirname, "m" + i))) {
-      continue;
-    } else {
-      fs.mkdirSync(path.join(__dirname, "m" + i));
-      const regex = new RegExp("m" + i + "-[0-9]{2}");
-      files.forEach(file => {
-        const arr = file.match(regex);
-        if (arr) {
-          fs.rename(
-            path.join(__dirname, arr.input),
-            path.join(
-              __dirname,
-              "m" + i,
-              file.substring(0, 2) === arr[0].split("-")[1]
-                ? "" + file
-                : arr[0].split("-")[1] + "-" + file
-            ),
-            error => {
-              if (error) throw err;
-            }
-          );
-        }
-      });
+  if (nullCount === files.length) {
+    newSort();
+    nullCount = 0;
+  } else {
+    chaps.sort();
+    for (i = chaps[0]; i <= chaps[chaps.length - 1]; i++) {
+      if (fs.existsSync(path.join(__dirname, "m" + i))) {
+        continue;
+      } else {
+        fs.mkdirSync(path.join(__dirname, "m" + i));
+        const regex = new RegExp("m" + i + "-[0-9]{2}");
+        files.forEach(file => {
+          const arr = file.match(regex);
+          if (arr) {
+            fs.rename(
+              path.join(__dirname, arr.input),
+              path.join(
+                __dirname,
+                "m" + i,
+                file.substring(0, 2) === arr[0].split("-")[1]
+                  ? "" + file
+                  : arr[0].split("-")[1] + "-" + file
+              ),
+              error => {
+                if (error) throw err;
+              }
+            );
+          }
+        });
+      }
     }
+    console.log("Files Sorted!!!");
   }
-  console.log("Files Sorted!!!");
 });
+newSort = () => {
+  fs.readdir(__dirname, (err, file) => {
+    file.sort((a, b) => {
+      const ta = fs.statSync(path.join(__dirname, a));
+      const tb = fs.statSync(path.join(__dirname, b));
+      return ta.birthtimeMs - tb.birthtimeMs;
+    });
+    file.forEach((fn, index) => {
+      if (fn === "script.js") return;
+      fs.renameSync(
+        path.join(__dirname, fn),
+        path.join(
+          __dirname,
+          index + " " + fn.replace(/[-][0-91-zA-Z]{8}(.*)(?=.mp4)/, "")
+        )
+      );
+    });
+  });
+  console.log("Files Sorted!!!");
+};
